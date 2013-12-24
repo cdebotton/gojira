5e5c501c72b44f39cdba3deb6addcc8ad1561b13
Christian de Botton <cdebotton@gmail.com>
2013-12-23 21:14:01 -0500

    Completed link-to helper with BDD.

--- app/helpers/link-to.js
+++ app/helpers/link-to.js
@@ -3,8 +3,7 @@
 module.exports = Handlebars.registerHelper('link-to', function(target, context, options) {
   var values = _.values(App.Router.routes),
       keys = _.keys(App.Router.routes),
-      reg = new RegExp('([^\/]+)/:([A-Za-z0-9_-]+)', 'ig'),
-      mock, index;
+      mock, index, model;
 
   if (options === void 0) {
     options = context;
@@ -14,26 +13,43 @@ module.exports = Handlebars.registerHelper('link-to', function(target, context, 
     }, this._parentView.model.toJSON());
   }
 
+  if (options.hash && options.hash.model) {
+    model = this._parentView.model.get(options.hash.model);
+  }
+  else {
+    model = this._parentView.model;
+  }
+
   mock = _.clone(context);
   delete mock._parentView;
   delete mock._cid;
-
   if ((index = _.indexOf(values, target)) > -1) {
-    var route = keys[index],
-        flat = App.Utils.flatten(mock);
-    if (reg.exec(route)) {
-      var queryParams = _.reduce(route.match(reg), function(memo, segment, index, matches) {
-        var segment = segment.split('/'),
-            key = segment[1];
-        console.log(memo, '\n', key);
-        return memo
-      }, {
-        objectPath: '',
-        routePath: route
-      });
-    }
-    var str = '<a href="/'+ route +'">' + options.fn() + '</a>';
-    return str;
+    var route = keys[index];
+    var objectPath = _.reduce(target.split('.'), _.bind(function(memo, part, key, array) {
+      var reg = new RegExp(':' + part +'_([^\/]+)', 'i'),
+          matches = route.match(reg),
+          path, property, localPath, value;
+      if (! matches) {
+        return array[0];
+      }
+
+      property = matches[1];
+
+      if (key > 0) {
+        memo.push(part);
+        localPath = memo.join('.') + '.' + property;
+        value = model.get(localPath);
+        route = route.replace(reg, value);
+      }
+      else {
+        value = model.get(property);
+        route = route.replace(reg, value);
+      }
+      return memo;
+    }, this), []);
+
+
+    return '<a href="/'+ route +'">' + options.fn() + '</a>';
   }
 
   throw new Error('route: ' + target + ' does not exist.');
--- test/helpers/linkToTest.js
+++ test/helpers/linkToTest.js
@@ -1,21 +1,51 @@
 describe('helpers/link-to', function() {
-  beforeEach(function() {
+  before(function() {
     var rootNode = document.createElement('root-node');
+
+    var ProfileModel = App.Model.extend({});
+
+    var UserModel = App.Model.extend({
+      relations: [{
+        key: 'profile',
+        relatedModel: ProfileModel,
+        type: 'HasOne'
+      }]
+    });
+
+    var UsersCollection = Backbone.Collection.extend({
+      model: UserModel
+    });
+
+    var AppModel = App.Model.extend({
+      relations: [{
+        relatedModel: UserModel,
+        collectionType: UsersCollection,
+        type: 'HasMany',
+        key: 'users',
+        reverseRelation: { includeInJSON: 'id' }
+      }]
+    });
+
     this.view = new App.ApplicationView({
       el: rootNode,
-      model: new App.ApplicationModel
+      model: new AppModel({
+        users: [{id: 1, profile: { id: 2, firstName: 'Christian' }}]
+      })
     });
-    delete App.Router;
-  });
-
-  afterEach(function() {
-    this.view.model.destroy();
-    this.view.remove();
   });
 
   after(function() {
     delete App.Router;
+    Backbone.history.handlers = [];
     App.Router = new App.ApplicationRouter;
+  });
+
+  beforeEach(function() {
+    delete App.Router;
+  });
+
+  afterEach(function() {
+
   });
 
   it('Should alias index to a root level URI.', function() {
@@ -37,5 +67,32 @@ describe('helpers/link-to', function() {
     }));
     this.view.template = Handlebars.compile('{{#link-to \'about\'}}About{{/link-to}}');
     expect(this.view.render().$('a').attr('href')).to.equal('/about');
+  });
+
+  it('Should print contained text in innerHTML', function() {
+    App.Router = new App.ApplicationRouter;
+    this.view.template = Handlebars.compile('{{#link-to \'index\'}}Test string{{/link-to}}');
+    this.view.render();
+    expect(this.view.$('a').html()).to.equal('Test string');
+  });
+
+  it('Should resolve query params', function() {
+    App.Router = new (App.ApplicationRouter.extend({
+      routes: {
+        'users': 'users',
+        'users/:user_id': 'user',
+        'users/:user_id/profile/:profile_id': 'user.profile'
+      }
+    }));
+
+    this.view.template = Handlebars.compile([
+      '{{#link-to \'users\'}}Users{{/link-to}}',
+      '{{#link-to \'user\' model=\'users.0\'}}First User{{/link-to}}',
+      '{{#link-to \'user.profile\' model=\'users.0\'}}First User\'s profile{{/link-to}}'
+    ].join('\n'));
+
+    expect(this.view.render().$('a').eq(0).attr('href')).to.equal('/users');
+    expect(this.view.render().$('a').eq(1).attr('href')).to.equal('/users/1');
+    expect(this.view.render().$('a').eq(2).attr('href')).to.equal('/users/1/profile/2');
   });
 });
