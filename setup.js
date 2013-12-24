68db6df35b74f2a063621e224451a0061167f6c1
Christian de Botton <cdebotton@gmail.com>
2013-12-24 00:44:26 -0500

    Adding execute to setup.js

--- setup.js
+++ setup.js
@@ -6,6 +6,17 @@ var exec = require('child_process').exec,
     mode = process.argv[2],
     fsExistSync = fs.fsExistSync || sysPath.fsExistSync;
 
+var execute = function(path, params, callback) {
+  if (callback == null) callback = function() {};
+  var command = path + ' ' + params;
+  console.log('Executing', command);
+  exec(command, function(error, stdout, stderr) {
+    if (error != null) return process.stderr.write(stderr.toString());
+    console.log(stdout.toString());
+    callback();
+  });
+};
+
 switch(mode) {
   case 'update':
     execute('curl https://codeload.github.com/cdebotton/whats-in-the-box/zip/master', '> master.zip', function() {
