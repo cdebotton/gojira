d4ad1cfa7149acaeaad383286036fd131ccdccf7
Christian de Botton <cdebotton@gmail.com>
2013-12-28 20:00:57 -0500

    Adding request and cancel animation frame shims to App.Utils

--- app/framework/config/utils.js
+++ app/framework/config/utils.js
@@ -17,7 +17,30 @@ module.exports = App.Utils = (function() {
     return into;
   };
 
+  var lastTime = 0;
+  var vendors = ['ms', 'moz', 'webkit', 'o'];
+  for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+      window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+      window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                 || window[vendors[x]+'CancelRequestAnimationFrame'];
+  }
+
+  var requestAnimationFrame = !!window.requestAnimationFrame ? window.requestAnimationFrame : function(callback, element) {
+    var currTime = new Date().getTime();
+    var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+    var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+      timeToCall);
+    lastTime = currTime + timeToCall;
+    return id;
+  };
+
+  var cancelAnimationFrame = !!window.cancelAnimationFrame ? window.cancelAnimationFrame : function(id) {
+    clearTimeout(id);
+  };
+
   return {
-    flatten: flatten
+    flatten: flatten,
+    requestAnimationFrame: requestAnimationFrame,
+    cancelAnimationFrame: cancelAnimationFrame
   };
 })();
